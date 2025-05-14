74842e74767d78ba18eae0534feef2e5d01c1488
João <dev.oliveira0235@gmail.com>
2025-05-14 00:50:48 -0400

    Update: Adicionando função list grupo e get product by id

--- padaria/lib/data/repositories/product_repository.dart
+++ padaria/lib/data/repositories/product_repository.dart
@@ -8,9 +8,9 @@ class ProductRepository {
     final response = await ApiProducts.post("/create-product", {
       'descricao': product.descricao,
       'valor': product.valor,
-      'status': product.status,
+      'status': product.estoque,
       'grupoId': product.grupoId,
-      'estoque': product.estoque,
+      'estoque': product.status,
     });
     return ProductModel.fromJson(response);
   }
@@ -21,20 +21,32 @@ class ProductRepository {
   }
 
   Future<ProductModel> updateProduct(ProductModel product) async {
-    final response = await ApiProducts.post('/update-products', {
+    final response = await ApiProducts.post('/update-product', {
       'objectId': product.objectId,
       'descricao': product.descricao,
       'valor': product.valor,
-      'status': product.status,
+      'status': product.estoque,
       'grupoId': product.grupoId,
-      'estoque': product.estoque,
+      'estoque': product.status,
     });
     return ProductModel.fromJson(response);
   }
 
-  Future<void> deleteProduct(String objesctId) async {
+  Future<void> deleteProduct(String objectId) async {
     await ApiProducts.post("/delete-product", {
-      'objectId': objesctId,
+      'objectId': objectId,
     });
+  }
+
+  Future<List<Map<String, dynamic>>> listGrupo() async {
+    final result = await ApiProducts.getList("/list-groups");
+    return List<Map<String, dynamic>>.from(result);
+  }
+
+  Future<ProductModel> getProductById(String objectId) async {
+    final response = await ApiProducts.post("/get-product", {
+      'objectId': objectId,
+    });
+    return ProductModel.fromJson(response);
   }
 }
